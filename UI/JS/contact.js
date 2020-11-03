40b8e942b0da868803b5c2f19bd065bc0447d99c
jeannettuwama <uwamahorojanet88@gmail.com>
2020-11-03 23:36:46 +0200

    ft-contactpage

---
+++ UI/JS/contact.js
@@ -0,0 +1,30 @@
+function validation(){
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message");
+    var text;
+    error_message.style.padding = "10px";
+    if(name.length <5){
+        text = "Please Enter Valid Name";
+        error_message.innerHTML = text;
+    return false;
+    }
+    if(email.indexOf("@")== -1 ||email.length<6){
+        text = "Please Enter Valid Email";
+        error_message.innerHTML = text;
+    return false;
+    }
+    if(isNaN(phone)||phone.length != 10){
+        text = "please Enter Valid phone Number";
+        error_message.innerHTML = text;
+        return false;
+    }
+    if(message.length <140){
+        text = "Please Enter Valid Message";
+        error_message.innerHTML = text;
+    return false;
+    
+}
+}
